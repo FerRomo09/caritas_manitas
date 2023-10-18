fb230430b6efdd750a7582962ea4cc5081bcb65b
Romo <fernandaeliromo@hotmail.com>
2023-10-18 14:29:55 -0600

    Modificaciones
    Modificaciones log_in

--- manitasAPP.mc/manitasAPP.mc/Views/LogInView.swift
+++ manitasAPP.mc/manitasAPP.mc/Views/LogInView.swift
@@ -11,7 +11,36 @@ struct LogInView: View {
     var body: some View {
         VStack{
             Text("Inicio de Sesión")
-        }
+        
+            ZStack(){
+                Image("Cuadro")
+                    .cornerRadius(24)
+                
+                HStack{
+                    VStack(alignment: .leading){
+                        HStack{Spacer()}
+                        Text("Hola,")
+                            .font(.system(size: 20))
+                            .fontWeight(.regular)
+                            .foregroundColor(Color("manitasNegro"))
+                        Text("Maruca Cantu") //remplazar con nombre de variable
+                            .font(.system(size: 30))
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color("manitasNegro"))
+                        HStack{Spacer()}
+                    }
+                    .padding(.leading, 40)
+                    Image("Avatar")
+                        .padding(.trailing, 30)
+                    
+                    
+                }
+            }
+            
+            Spacer()
+            
+        }.background(Color("manitasAzul"))
+        
     }
 }
 
