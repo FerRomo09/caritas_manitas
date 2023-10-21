30645775702b72e494731ead4fb166c52f50b414
Jacobo Hirsch Rodriguez <129253801+JacoboHiro@users.noreply.github.com>
2023-10-20 18:10:08 -0600

    unica carpeta api

--- API/apiEndPoints.py
+++ API/apiEndPoints.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, HTTPException, status
 import pyodbc
 import hashing as hs
 import apiModels as am
+import datetime
+
 
 # from jwtTokens import create_jwt_token, get_current_user
 
@@ -119,8 +121,105 @@ async def confirm_order(orderID: int, order_data: am.OrderConfirmation):
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail="Internal Server Error")
+    
+    
+    
+    
+
+#funcion para modificar unas de las llaves del diccionario  
+def modificar_clave(diccionario, vieja_clave, nueva_clave):
+    return {clave if clave != vieja_clave else nueva_clave: valor for clave, valor in diccionario.items()}
+
+
+
+#endpoint que regresa en formato json las ordenes filtradas por el id del empleado  
+@app.get("/ordenes/{ID_EMPLEADO}")
+def llamar_ordenes(ID_EMPLEADO: int):
+    try:
+        with get_db_connection() as conn:
+            conn = get_db_connection()
+            cursor = conn.cursor()
+            cursor.execute(
+                "SELECT * FROM OPE_ORDENES WHERE ID_EMPLEADO = ?", (ID_EMPLEADO,))
+            resultados = cursor.fetchall()
+            if len(resultados) != 0:
+                ordenes_list = []
+                for item in resultados:
+
+                    data = {column[0]: value for column,
+                            value in zip(cursor.description, item)}
+
+                    # model_instance = am.Orden(**data)
+
+                    data = modificar_clave(data, 'ID_ORDEN', 'id')
+
+                    ordenes_list.append(data)
+
+                    # print(type(model_instance))
+
+            else:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                    detail=f"orders with id: {ID_EMPLEADO} does not exists")
+
+            cursor.close()
+            conn.close()
+
+        return {"mensaje": "ordenes llamadas exitosamenteeee", "list:": ordenes_list}
+
+    # Asumiendo que DatabaseConnectionError es una excepción que podría lanzar get_db_connection().
+    except pyodbc.Error:
+        raise HTTPException(
+            status_code=503, detail="No se pudo establecer conexión con la base de datos.")
+
+    except Exception as error_name:
+        raise HTTPException(status_code=500, detail=str(error_name))
+
+
+
+
+# Endpoint para cambiar la informacion de las ordenes reprogramadas 
+@app.put("/reprogram_order/{orderID}")
+def reprogram_order(orderID: int,  info_body: am.Reprogramacion):
+
+    comentarios = info_body.comentarios
+
+    try:
+        with get_db_connection() as conn:
+            
+            current_date = datetime.date.today()
+            current_date = current_date.strftime("%Y-%m-%d")
+            conn = get_db_connection()
+            cursor = conn.cursor()
+
+            print("hola4")
+            print(current_date)
+            print(type(current_date))
+
+           
+
+
+            cursor.execute("UPDATE OPE_ORDENES SET FECHA_VISITA = ?, COMENTARIOS_REPROGRAMACION = ? WHERE ID_ORDEN = ?",
+                           (current_date, comentarios, orderID))
+            conn.commit()
+
+
+        cursor.close()
+        conn.close()
+
+        return {"message": "Reprogramacion confirmada"}
+
+    except pyodbc.Error:
+        raise HTTPException(
+            status_code=503, detail="No se pudo establecer conexión con la base de datos.")
+
+    except Exception as error_name:
+        raise HTTPException(status_code=500, detail=str(error_name))
+
 
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("apiEndPoints:app", host="0.0.0.0", port=8037, reload=True)
+
+
+
