b4c6deb5872d174fdeaca1377da9fa26bd522ade
Jacobo Hirsch Rodriguez <129253801+JacoboHiro@users.noreply.github.com>
2023-11-08 23:06:48 -0600

    cmabio de query a stored procedure endp ordbyid

--- API/apiEndPoints.py
+++ API/apiEndPoints.py
@@ -17,6 +17,7 @@ conn_str = (
     "UID=SA;"
     "PWD=Shakira123.;"
     "TrustServerCertificate=yes"  
+    
 )
 
 # Function to establish a database connection
@@ -140,7 +141,7 @@ def llamar_ordenes(ID_EMPLEADO: int):
             conn = get_db_connection()
             cursor = conn.cursor()
             cursor.execute(
-                "SELECT * FROM OPE_ORDENES WHERE ID_EMPLEADO = ?", (ID_EMPLEADO,))
+                "EXEC SelectOrderById @EmpleadoID = ?;", (ID_EMPLEADO,))
             resultados = cursor.fetchall()
             if len(resultados) != 0:
                 ordenes_list = []
@@ -154,6 +155,7 @@ def llamar_ordenes(ID_EMPLEADO: int):
                     data = modificar_clave(data, 'ID_ORDEN', 'id')
 
                     ordenes_list.append(data)
+            
 
                     # print(type(model_instance))
 
@@ -191,11 +193,6 @@ def reprogram_order(orderID: int,  info_body: am.Reprogramacion):
             conn = get_db_connection()
             cursor = conn.cursor()
 
-            print("hola4")
-            print(current_date)
-            print(type(current_date))
-
-           
 
 
             cursor.execute("UPDATE OPE_ORDENES SET FECHA_VISITA = ?, COMENTARIOS_REPROGRAMACION = ? WHERE ID_ORDEN = ?",
@@ -220,6 +217,7 @@ def reprogram_order(orderID: int,  info_body: am.Reprogramacion):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("apiEndPoints:app", host="0.0.0.0", port=8037, reload=True)
+
 
 
 
