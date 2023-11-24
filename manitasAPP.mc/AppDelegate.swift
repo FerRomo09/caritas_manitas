502feb7f098f43813c609f6a2431ff292c9a1174
Maruca Cantu Valdes <89872076+A00834245@users.noreply.github.com>
2023-11-24 11:25:02 -0600

    errores en app delegate

---
+++ manitasAPP.mc/AppDelegate.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    var crashDetectionManager: CrashDetectionManager?
+    var backgroundTask: UIBackgroundTaskIdentifier = .invalid
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        crashDetectionManager = CrashDetectionManager()
+        return true
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Resume motion updates when the app returns to the foreground
+        crashDetectionManager?.startMotionUpdates()
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Stop motion updates when the app is terminated
+        crashDetectionManager?.stopMotionUpdates()
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Start a background task to ensure continuous execution
+        backgroundTask = application.beginBackgroundTask {
+            // Cleanup code if the task is interrupted
+            application.endBackgroundTask(self.backgroundTask)
+            self.backgroundTask = .invalid
+        }
+
+        // Ensure the crash detection manager continues to run in the background
+        crashDetectionManager?.startMotionUpdates()
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Stop the background task when the app comes to the foreground
+        if backgroundTask != .invalid {
+            application.endBackgroundTask(backgroundTask)
+            backgroundTask = .invalid
+        }
+
+        // Resume motion updates
+        crashDetectionManager?.startMotionUpdates()
+    }
+}
