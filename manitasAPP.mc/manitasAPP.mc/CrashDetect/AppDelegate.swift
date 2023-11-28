b08972c1e341f4e43bcd0909481610f2bbad4677
F. N. Jervis <nicojervis@gmail.com>
2023-11-28 13:15:36 -0600

    .

--- manitasAPP.mc/manitasAPP.mc/CrashDetect/AppDelegate.swift
+++ manitasAPP.mc/manitasAPP.mc/CrashDetect/AppDelegate.swift
@@ -1,8 +1,41 @@
-//
-//  AppDelegate.swift
-//  manitasAPP.mc
-//
-//  Created by Alumno on 28/11/23.
-//
 
-import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    var crashDetectionManager: CrashDetectionManager?
+    var backgroundTask: UIBackgroundTaskIdentifier = .invalid
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        crashDetectionManager = CrashDetectionManager()
+        return true
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Stop the background task when the app comes back to the foreground
+        if backgroundTask != .invalid {
+            application.endBackgroundTask(backgroundTask)
+            backgroundTask = .invalid
+        }
+
+        // Resume motion updates
+        crashDetectionManager?.startMotionUpdates()
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Start a background task to ensure continuous execution
+        backgroundTask = application.beginBackgroundTask {
+            // Cleanup code if the task is interrupted
+            application.endBackgroundTask(self.backgroundTask)
+            self.backgroundTask = .invalid
+        }
+
+        // Ensure the crash detection manager continues to run in the background
+        crashDetectionManager?.startMotionUpdates()
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Stop motion updates when the app is terminated
+        crashDetectionManager?.stopMotionUpdates()
+    }
+}
+
